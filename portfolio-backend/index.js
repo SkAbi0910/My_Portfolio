0704067226ecf71146f09048054b9e00bdb65e97
Abinaya <abinaya2001dev@gmail.com>
2025-09-20 15:21:23 +0530

    MongoDB Committed

--- portfolio-backend/index.js
+++ portfolio-backend/index.js
@@ -14,11 +14,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
+mongoose.connect(process.env.MONGO_URI)
+  
+.then(() =>
+    { console.log("MongoDB connected");
+    app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
 })
-.then(() => console.log("MongoDB connected"))
 .catch(err => console.error("MongoDB Connection Error: " , err));
 
 app.use("/api/projects", projectRoutes);
@@ -30,6 +33,3 @@ app.get("/", (req,res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
